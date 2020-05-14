854aadec6d4d6d62b6444fa1331692fc04f629ec
unknown <haniehmahdavi@yahoo.com>
2020-05-14 22:40:41 +0430

    CORS just added

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
-
+from flask_cors import CORS
 from resources.errors import errors
 from database.db import initialize_db
 
